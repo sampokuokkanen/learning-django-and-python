c3e8237eb49094162619dab3a5772a4c5cb8503c
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-01-21 23:45:36 +0900

    some new functional tests, a bit of a code smell too

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-
 from lists.views import home_page
 
 
@@ -8,3 +7,9 @@ class HomePageTest(TestCase):
     def test_root_urls_resolves_to_home_page_view(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
