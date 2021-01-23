81986dad473cf7e813ed78feca5f9eea4729e1b8
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-01-23 22:23:58 +0900

    a bit more functional testing

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -55,3 +55,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('finish the test')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # she notices her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now a new user! Francis
+
+        ## New browser session
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits homepage, no sign of Edith's visit
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assetNotIn('Buy peacock feathers', page_text)
+        self.assetNotIn('make a fly', page_text)
+
+        # Francis starts a new list
+
