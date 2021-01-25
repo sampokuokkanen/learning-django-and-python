f5c19c81b84f96907cb63950702fc91a21214dfd
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-01-25 22:41:22 +0900

    Now the tests look quite a bit better, a new url for list creation

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from lists.views import home_page
 from lists.models import Item
 
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -24,11 +25,9 @@ class ItemModelTest(TestCase):
 
 class ListViewTest(TestCase):
 
-
     def test_user_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
-
 
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
@@ -39,24 +38,21 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
+
 class HomePageTest(TestCase):
     def test_root_urls_resolves_to_home_page_view(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
 
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_post(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
