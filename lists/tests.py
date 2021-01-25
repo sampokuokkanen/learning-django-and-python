0fdd8d221bbf4165f5e596aa3f9060cbce5cb368
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-01-25 23:36:58 +0900

    lists are now almost working

--- lists/tests.py
+++ lists/tests.py
@@ -35,18 +35,25 @@ class ListAndItemsModelTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_user_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        other_list = List.objects.create()
+        Item.objects.create(text='other item 1', list=other_list)
+        Item.objects.create(text='other item 2', list=other_list)
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other item 1')
+        self.assertNotContains(response, 'other item 2')
 
 
 class HomePageTest(TestCase):
@@ -65,4 +72,5 @@ class NewListTest(TestCase):
 
     def test_redirects_after_post(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        list_ = List.objects.first()
+        self.assertRedirects(response, f'/lists/{list_.id}/')
