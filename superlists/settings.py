0db92aee0c099fedc6e3bfdfd2d8a98d39503c48
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-01-30 18:58:51 +0900

    changed env variables

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,14 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6si1-f2&_^#@+(2-1xx--&nvdrx$wt*+mvodxtz+l3r^ovx%rp'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    SECRET_KEY = '6si1-f2&_^#@+(2-1xx--&nvdrx$wt*+mvodxtz+l3r^ovx%rp'
+    DEBUG = True
+    ALLOWED_HOSTS = []
 
 # Application definition
 
