4db4b8c3b9fd1301f6e81e2eb3d4cfd76c38f067
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-02-02 22:38:39 +0900

    form objects

---
+++ lists/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from lists.models import Item
+
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = {'text',}
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+            'placeholder': 'Enter a to-do item',
+            'class': 'border py-2 px-3 text-grey-darkest',
+            })
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
