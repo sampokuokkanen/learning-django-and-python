d2498543e28d7136cee2e8963c6b81a636497ac0
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-02-09 23:04:36 +0900

    new accounts module

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from lists import views
 from lists import urls as list_urls
+from accounts import urls as account_urls
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
+    url(r'^accounts/', include(account_urls)),
 ]
