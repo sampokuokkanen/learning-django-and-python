df360dd93f8d5d9fb42a2b786977443bda329397
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-02-22 23:21:08 +0900

    update channel name

--- superlists/asgi.py
+++ superlists/asgi.py
@@ -1,5 +1,9 @@
 import os
-import channels
+import django
+from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-channel_layer = channels.asgi.get_channel_layer()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
+
+django.setup()
+
+application = get_default_application()
